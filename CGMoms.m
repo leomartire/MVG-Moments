2ed85b2a84513bf5d6e538d53ebeaabe70e69d81
Léo Martire <30411754+leomartire@users.noreply.github.com>
2017-07-24 16:42:21 +0200

    Update CGMoms.m

--- CGMoms.m
+++ CGMoms.m
@@ -1,8 +1,6 @@
 % Author:        Léo Martire.
 % Mail:          leo.martire@outlook.com
 % Description:   See below.
-% Last modified: 24/07/17.
-% Usage:         N/A.
 % Notes:         Requires scripts "CGMoms_Kan.m"
 %                                   "vectorial_nchoosek.m"
 
